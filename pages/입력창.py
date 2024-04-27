c47e44d6053493a74f0e2ea3991dbba6e745b8ab
lime122613 <167674932+lime122613@users.noreply.github.com>
2024-04-27 02:00:34 +0000

    입력창 꾸미기

--- pages/입력창.py
+++ pages/입력창.py
@@ -3,7 +3,7 @@ import random
 
 st.title("여러 가지 입력창")
 
-import streamlit as st
+st.write("<h4>1.랜덤 숫자 생성</h4>", unsafe_allow_html=True)
 
 st.button("리셋 버튼", type="primary")
 if st.button('랜덤 숫자 생성'):
@@ -13,13 +13,12 @@ else:
 
 
 
-text_contents = '''This is some text'''
-st.download_button('Download some text', text_contents)
+st.write("<h4>2.하이퍼링크</h4>", unsafe_allow_html=True)
+st.link_button("세화여고 리로스쿨 바로가기", "https://sehwags.riroschool.kr/")
 
 
-st.link_button("Go to gallery", "https://streamlit.io/gallery")
-
-
+st.write("<h4>3.체크박스</h4>", unsafe_allow_html=True)
+st.write("<개인정보 동의서>")
 agree = st.checkbox('동의합니다.')
 
 if agree:
@@ -27,35 +26,35 @@ if agree:
 else:
     st.write('동의 버튼을 먼저 클릭해주세요.')
 
-
-import streamlit as st
-
+st.write("<h4>4.여러 선택지</h4>", unsafe_allow_html=True)
 options = st.multiselect(
-    'What are your favorite colors',
+    '가장 좋아하는 색깔은?',
     ['Green', 'Yellow', 'Red', 'Blue'], #선택지
     ['Yellow', 'Red']) #기본 선택 옵션
 
-st.write('You selected:', options)
+st.write('당신의 선택은:', options)
 
-
+st.write("<h4>5.객관식 선택</h4>", unsafe_allow_html=True)
 genre = st.radio(
-    "What's your favorite movie genre",
-    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    "가장 좋아하는 영화 장르를 선택해주세요",
+    [":rainbow[로맨틱코미디]", "***액션***", "다큐멘터리 :movie_camera:"],
     index=None,
 )
 
-st.write("You selected:", genre)
+st.write("당신은", genre,"장르를 좋아하시는군요!")
 
 
+st.write("<h4>6.숫자 간격</h4>", unsafe_allow_html=True)
+n = st.slider('숫자의 증/감 간격을 설정해주세요', 0, 100)
+number = st.number_input('숫자를 선택해주세요.',step=n)
+st.write('현재 숫자: ', number)
 
-number = st.number_input('숫자를 입력해주세요.',step=1)
-st.write('The current number is ', number)
 
-
-
-title = st.text_area('Movie title', 'Life of Brian') #큰 텍스트 
+st.write("<h4>7.긴 글 입력창</h4>", unsafe_allow_html=True)
+title = st.text_area('영화 제목 작성', 'Life of Brian') #큰 텍스트 
 st.write(title,'을 보셨군요! 저도요!')
 
+st.write("<h4>8.csv 파일 업로드</h4>", unsafe_allow_html=True)
 import pandas as pd
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
