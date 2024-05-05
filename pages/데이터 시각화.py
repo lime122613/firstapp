01b124e5dee2d0fdc59facc4410dbe5ca0caab6a
lime122613 <167674932+lime122613@users.noreply.github.com>
2024-05-05 03:37:05 +0000

    add: 데이터 식가화, 입력창 기능 추

---
+++ pages/데이터 시각화.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("데이터 시각화")
+
+st.write('데이터에 따른 시각화 ')
+csv_file = st.file_uploader(label = "임의의 데이터를 업로드해주세요. 단, csv 파일만 가능합니다. ")
+if csv_file is not None:
+    csv_file_df = pd.read_csv(csv_file,encoding="euc-kr") 
+    st.write(csv_file_df.head(5))
+
+column = st.radio(label="열 이름을 선택해주세요.",options = csv_file_df.columns)
+st.subheader(column,'의 분포를 그려보겠습니다!')
+st.bar_chart(csv_file_df[column].value_counts())
+
+
+fig, ax = plt.subplots()
+sns.histplot(csv_file_df[column], binrange=[2500,7000],binwidth=100) #seaborn으로 
+st.pyplot(fig)
+
+st.write('막대 그래프')
+csv_file = st.file_uploader(label = "펭귄 데이터를 업로드해주세요. 단, csv 파일만 가능합니다. ")
+if csv_file is not None:
+    csv_file_df = pd.read_csv(csv_file,encoding="euc-kr") 
+    st.write(csv_file_df.head(5))
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.write(chart_data)
+st.write(csv_file_df['species'].value_counts())
+st.bar_chart(csv_file_df['species'].value_counts())
+st.bar_chart(csv_file_df['island'].value_counts())
+
